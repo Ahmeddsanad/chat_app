6d0c06f9b716cdd10288f0d6aa191636480e9ec0
Ahmed Sanad <ahmed.hac26@gmail.com>
2023-08-05 20:59:32 +0300

    finish_login_screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -4,126 +4,163 @@ import 'package:chat_app/components/TextItem.dart';
 import 'package:chat_app/components/buttonItem.dart';
 import 'package:chat_app/constant.dart';
 import 'package:chat_app/screens/register_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   LoginScreen({super.key});
 
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   String id = 'LoginScreen';
 
   TextEditingController emailController = TextEditingController();
+
   TextEditingController passwordController = TextEditingController();
+
+  String? email;
+
+  String? password;
+
+  bool isLoading = false;
 
   GlobalKey<FormState> formKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kPrimaryColor,
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Form(
-          key: formKey,
-          child: Center(
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 75.0,
-                ),
-                Image.asset('assets/images/scholar.png'),
-                const Text(
-                  'Scholar Chat',
-                  style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    color: Colors.white,
-                    fontSize: 32.0,
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        backgroundColor: kPrimaryColor,
+        body: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Form(
+            key: formKey,
+            child: Center(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 75.0,
                   ),
-                ),
-                const SizedBox(
-                  height: 75.0,
-                ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 24.0),
-                  child: Row(
-                    children: [
-                      Text(
-                        'LOGIN',
-                        style: TextStyle(
-                          fontFamily: 'RobotoCondensed',
-                          color: Colors.white,
-                          fontSize: 32.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 15.0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                  child: TextItem(
-                    validator: (data) {
-                      if (data!.isEmpty) {
-                        return 'email must be filled';
-                      }
-                    },
-                    text: 'Email',
-                    controller: emailController,
-                  ),
-                ),
-                const SizedBox(
-                  height: 15.0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                  child: TextItem(
-                    validator: (data) {
-                      if (data!.isEmpty) {
-                        return 'password must be filled';
-                      }
-                    },
-                    text: 'Password',
-                    controller: passwordController,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                ButtonItem(
-                    function: () {
-                      if (formKey.currentState!.validate()) {}
-                    },
-                    text: 'Login',
-                    horizontalSymmetric: 150.0),
-                const SizedBox(
-                  height: 15.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'don\'t have an account?',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18.0,
-                      ),
+                  Image.asset('assets/images/scholar.png'),
+                  const Text(
+                    'Scholar Chat',
+                    style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      color: Colors.white,
+                      fontSize: 32.0,
                     ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(context, RegisterScreen.id);
+                  ),
+                  const SizedBox(
+                    height: 75.0,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 24.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          'LOGIN',
+                          style: TextStyle(
+                            fontFamily: 'RobotoCondensed',
+                            color: Colors.white,
+                            fontSize: 32.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: TextItem(
+                      onChanged: (data) {
+                        email = data;
                       },
-                      child: const Text(
-                        ' Register',
+                      validator: (data) {
+                        if (data!.isEmpty) {
+                          return 'email must be filled';
+                        }
+                      },
+                      text: 'Email',
+                      controller: emailController,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: TextItem(
+                      onChanged: (data) {
+                        password = data;
+                      },
+                      validator: (data) {
+                        if (data!.isEmpty) {
+                          return 'password must be filled';
+                        }
+                      },
+                      text: 'Password',
+                      controller: passwordController,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  ButtonItem(
+                      function: () async {
+                        if (formKey.currentState!.validate()) {
+                          setState(() {
+                            isLoading = true;
+                          });
+
+                          UserCredential user = await FirebaseAuth.instance
+                              .signInWithEmailAndPassword(
+                            email: email!,
+                            password: password!,
+                          );
+                          setState(() {
+                            isLoading = false;
+                          });
+                          print('login success');
+                        }
+                      },
+                      text: 'Login',
+                      horizontalSymmetric: 150.0),
+                  const SizedBox(
+                    height: 15.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'don\'t have an account?',
                         style: TextStyle(
-                          color: Color(0xffc7ede6),
+                          color: Colors.white,
                           fontSize: 18.0,
                         ),
                       ),
-                    )
-                  ],
-                ),
-              ],
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, RegisterScreen.id);
+                        },
+                        child: const Text(
+                          ' Register',
+                          style: TextStyle(
+                            color: Color(0xffc7ede6),
+                            fontSize: 18.0,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
