3fd3565b94f3d53b48543b814f42069fa7d39a52
Ahmed Sanad <ahmed.hac26@gmail.com>
2023-08-07 16:09:07 +0300

    adding_data_firebase

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,11 +1,22 @@
 import 'package:chat_app/components/chat_bubble.dart';
 import 'package:chat_app/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class ChatScreen extends StatelessWidget {
-  const ChatScreen({super.key});
+  ChatScreen({super.key});
+
+  TextEditingController ChatController = TextEditingController();
+
+  CollectionReference messages =
+      FirebaseFirestore.instance.collection('messages');
+
+  var CurrentTime = DateFormat('yyyy-MM-dd – hh:mm a').format(DateTime.now());
 
   static String id = 'ChatScreen';
+
+  String? message;
 
   @override
   Widget build(BuildContext context) {
@@ -34,13 +45,25 @@ class ChatScreen extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextFormField(
+              controller: ChatController,
+              onFieldSubmitted: (data) {
+                messages.add({
+                  'message': data,
+                  'DateTime': CurrentTime,
+                });
+
+                ChatController.clear();
+              },
+              onChanged: (value) {
+                message = value;
+              },
               decoration: InputDecoration(
                   hintText: 'Send Message',
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(
                       16.0,
                     ),
-                    borderSide: BorderSide(
+                    borderSide: const BorderSide(
                       color: kPrimaryColor,
                     ),
                   ),
@@ -48,13 +71,23 @@ class ChatScreen extends StatelessWidget {
                     borderRadius: BorderRadius.circular(
                       24.0,
                     ),
-                    borderSide: BorderSide(
+                    borderSide: const BorderSide(
                       color: Colors.teal,
                     ),
                   ),
-                  suffixIcon: Icon(
-                    Icons.send,
-                    color: Colors.teal,
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      messages.add({
+                        'message': message,
+                        'DateTime': CurrentTime,
+                      });
+
+                      ChatController.clear();
+                    },
+                    icon: const Icon(
+                      Icons.send,
+                      color: Colors.teal,
+                    ),
                   )),
             ),
           ),
